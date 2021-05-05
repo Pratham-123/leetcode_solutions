017b444519b393f91fc66a65798d9b5c8108d9c6
Pratham-123 <54859695+Pratham-123@users.noreply.github.com>
2021-05-06 00:06:39 +0530

    Update longest_substring_without_repeating_characters.cpp

--- longest_substring_without_repeating_characters.cpp
+++ longest_substring_without_repeating_characters.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if(s.size()==0) return 0;
+        if(s.size()==0) return 0 ;
         
-        int n = s.size();
+        int n = s.size() ;
         
         int ans=1;
         
